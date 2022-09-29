99a18bc79af30eb37bbb7664c0c8896acc279832
Rahul Patel <63693793+Rahul5798@users.noreply.github.com>
2022-09-29 19:06:59 -0400

    Login validation & session completed

--- js/login.js
+++ js/login.js
@@ -11,3 +11,30 @@ if (date.getHours() < 12) {
 } else if (date.getHours() > 16) {
     document.getElementById("wlcmMsg").innerHTML = "Good Evening!" + wlcmMsg;
 }
+
+//javascript for login
+function formValidation() {
+
+    if (document.getElementById("userName").value != "") {
+        if (document.getElementById("password").value != "") {
+            if (document.getElementById("userName").value == "User1223") {
+                if (document.getElementById("password").value == "User@1223") {
+                    sessionStorage.setItem("loggedIn", 1);
+                    return true;
+                } else {
+                    document.getElementById("errorMsg").innerHTML = "Incorrect password! Please, try again."
+                    return false;
+                }
+            } else {
+                document.getElementById("errorMsg").innerHTML = "Incorrect username! Please, try again."
+                return false;
+            }
+        } else {
+            document.getElementById("errorMsg").innerHTML = "Password cannot be empty!";
+            return false;
+        }
+    } else {
+        document.getElementById("errorMsg").innerHTML = "Username cannot be empty!";
+        return false;
+    }
+}
