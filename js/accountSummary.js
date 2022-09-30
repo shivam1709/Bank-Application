6ce5f2087cf2398cb2d71ea7b924fa4ef9177776
shivam1709 <68727150+shivam1709@users.noreply.github.com>
2022-09-30 12:39:51 -0400

    account summary screen html done

---
+++ js/accountSummary.js
@@ -0,0 +1,17 @@
+function accountInfo(evt, accountType) {
+    var i, type, links;
+  
+    type = document.getElementsByClassName("type");
+    for (i = 0; i < type.length; i++) {
+        type[i].style.display = "none";
+    }
+  
+
+links = document.getElementsByClassName("links");
+  for (i = 0; i < links.length; i++) {
+        links[i].className = links[i].className.replace(" active", "");
+}
+  
+ document.getElementById(accountType).style.display = "block";
+  evt.currentTarget.className += " active";
+      }
