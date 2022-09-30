4181745f787a3881267adc5ed3132cdd9ebb8eb4
shivam1709 <68727150+shivam1709@users.noreply.github.com>
2022-09-30 12:52:10 -0400

    login html code added

--- js/accountSummary.js
+++ js/accountSummary.js
@@ -1,3 +1,10 @@
+if (sessionStorage.getItem("loggedIn") == 1) {
+    sessionStorage.setItem("user", "User1223");
+    document.getElementById("user").innerHTML = sessionStorage.getItem("user");
+} else {
+    window.location.replace("login.html")
+}
+
 function accountInfo(evt, accountType) {
     var i, type, links;
   
