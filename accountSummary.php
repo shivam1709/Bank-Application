c9e7d7ac8c88dbffaa69ea887896ea7ab1d6c14b
Rahul Patel <63693793+Rahul5798@users.noreply.github.com>
2022-10-14 18:53:44 -0400

    account summary done

--- accountSummary.php
+++ accountSummary.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< Updated upstream
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -71,6 +72,54 @@ $conn->close();
 
 ?>
 
+=======
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbssrdsbank";
+
+$email = $_SESSION['email'];
+$senders=array();
+$plusAmount=array();
+$minusAmount=array();
+$recievers=array();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM transactions WHERE EmailID = '$email' AND AccountType = 'chequing'";
+
+$result =  $conn->query($sql);
+    if ($result->num_rows >0) {
+       while($row = $result->fetch_assoc()) {
+            if($row["Sender"]!=""){
+                array_push($senders,$row["Sender"]);
+                array_push($plusAmount,$row["Amount"]);
+            }
+            else{
+                array_push($recievers,$row["Receiver"]);
+                array_push($minusAmount,$row["Amount"]);
+            }
+       }
+    }
+    else{
+        header("location:logout.php");
+    }
+?>
+>>>>>>> Stashed changes
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -99,7 +148,12 @@ $conn->close();
                 <a  href="logout.php">Logout </a>
             </div>
             <div class="logoutLink">
+<<<<<<< Updated upstream
                 <p><?php echo $name ?></p>  
+=======
+                <p id="user"><?php echo($_SESSION["username"]);?></p>
+               
+>>>>>>> Stashed changes
             </div>
         </div>
     </header>
@@ -114,10 +168,11 @@ $conn->close();
                     <h1>Account Balance</h1>
                 </div>
                 <div>
-                    <h1>$5000</h1>
+                    <h3><?php echo(array_sum($plusAmount)-array_sum($minusAmount));?></h3>
                 </div>
              </div>
             
+<<<<<<< Updated upstream
             <div class="brb">
                 <div class="transactionHistory">
                     <p><?php echo $chequing ?></p>
@@ -128,12 +183,56 @@ $conn->close();
                     <p class="plus"> <?php echo $chequingAmount?></p>
                 </div>
                 
+=======
+            <?php
+            echo "<div class='brb'>";
+            for($i=0;$i < count($senders);$i++){
+                echo"<div class='transactionHistory'>";
+                echo($senders[$i]);
+                echo"<p class='plus'>";
+                echo($plusAmount[$i]);
+                echo"</p></div>";
+            }
+            for($i=0;$i < count($recievers);$i++){
+                echo"<div class='transactionHistory'>";
+                echo($recievers[$i]);
+                echo"<p class='minus'>";
+                echo($minusAmount[$i]);
+                echo"</p></div>";
+            }?>
+>>>>>>> Stashed changes
             </div>
-            
-
         </div>
+            
+<?php
+$sendersSB=array();
+$plusAmountSB=array();
+$minusAmountSB=array();
+$recieversSB=array();
+$sql1 = "SELECT * FROM transactions WHERE EmailID = '$email' AND AccountType = 'saving'";
+
+$result1 =  $conn->query($sql1);
+    if ($result1->num_rows >0) {
+       while($row = $result1->fetch_assoc()) {
+            if($row["Sender"]!=""){
+                array_push($sendersSB,$row["Sender"]);
+                array_push($plusAmountSB,$row["Amount"]);
+            }
+            else{
+                array_push($recieversSB,$row["Receiver"]);
+                array_push($minusAmountSB,$row["Amount"]);
+            }
+       }
+    }
+    else{
+        header("location:logout.php");
+    }
+?>            
+
+        
         <div id="Saving" class="type">
             <div class="accountAmount">
+<<<<<<< Updated upstream
             <h1>Account Balance</h1>
             <h3>$1070</h3>
             </div>
@@ -148,6 +247,31 @@ $conn->close();
             </div>
            
             </div>
+=======
+                <div>
+                    <h1>Account Balance</h1>
+                </div>
+                <div>
+                    <h3><?php echo(array_sum($plusAmountSB)-array_sum($minusAmountSB));?></h3>
+                </div>
+             </div>
+            <?php
+            echo "<div class='brb'>";
+            for($j=0;$j < count($sendersSB);$j++){
+                echo"<div class='transactionHistory'>";
+                echo($sendersSB[$j]);
+                echo"<p class='plus'>";
+                echo($plusAmountSB[$j]);
+                echo"</p></div>";
+            }
+            for($j=0;$j < count($recieversSB);$j++){
+                echo"<div class='transactionHistory'>";
+                echo($recieversSB[$j]);
+                echo"<p class='minus'>";
+                echo($minusAmountSB[$j]);
+                echo"</p></div>";
+            }?>
+>>>>>>> Stashed changes
         </div>   
 
     </main>
--- js/accountSummary.js
+++ js/accountSummary.js
@@ -1,26 +1,19 @@
-if (sessionStorage.getItem("loggedIn") == 1) {
-    sessionStorage.setItem("user", "user123");
-    document.getElementById("user").innerHTML = sessionStorage.getItem("user");
-} else {
-    window.location.replace("login.php")
-}
-
 document.getElementById("defaultAccount").click();
 
 function accountInfo(evt, accountType) {
     var i, type, links;
-  
+
     type = document.getElementsByClassName("type");
     for (i = 0; i < type.length; i++) {
         type[i].style.display = "none";
     }
-  
 
-links = document.getElementsByClassName("links");
-  for (i = 0; i < links.length; i++) {
+
+    links = document.getElementsByClassName("links");
+    for (i = 0; i < links.length; i++) {
         links[i].className = links[i].className.replace(" active", "");
+    }
+
+    document.getElementById(accountType).style.display = "block";
+    evt.currentTarget.className += " active";
 }
-  
- document.getElementById(accountType).style.display = "block";
-  evt.currentTarget.className += " active";
-      }
